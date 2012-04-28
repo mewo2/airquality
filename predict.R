c71b7715d6cd51f3432f2945a1f463988f15e750
Martin O'Leary <m.e.w.oleary@gmail.com>
2012-04-28 18:43:06 -0400

    Slightly less stupider

--- predict.R
+++ predict.R
@@ -10,14 +10,35 @@ rmedian <- function (x) median(x, na.rm=T);
 
 medians <- colwise(rmedian, is.numeric)(train);
 
-preds <- lapply(c('hour', 'chunkID', 'month_most_common'), function (var) {
+mae <- function (x, y) {mean(abs(x - y))};
+
+varsets <- list(
+    NULL,
+    'hour',
+    'chunkID',
+    'month_most_common',
+    c('hour', 'chunkID'),
+    c('hour', 'month_most_common')
+  );
+preds <- lapply(varsets, function (var) {
   medians.by.var <- ddply(train, var, colwise(rmedian, is.numeric));
-  join(submit[,1:5], medians.by.var, by=var);
+  list(
+    submit=join(submit[,1:5], medians.by.var, by=var),
+    train=join(train[,1:6], medians.by.var, by=var),
+    );
 });
+
 
 for (target in colnames(submit)[6:44]) {
   idxs <- which(submit[,target] > -999999);
-  submit[idxs, target] <- apply(sapply(preds, function (pred) pred[idxs,target]), 1, rmedian);
+  cat(target, '\n');
+  maes <- sapply(preds, function (pred) {
+    m <- mae(pred$train[idxs, target], train[idxs, target]);
+    cat(' ', m, '\n');
+    m;
+    });
+    
+  submit[idxs, target] <- apply(sapply(preds, function (pred) pred$submit[idxs,target]), 1, rmedian);
 }
 
 argv <- commandArgs(T);
