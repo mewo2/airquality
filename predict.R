e2de76476924d040d4b30095aeb6122a01b029ce
Martin O'Leary <m.e.w.oleary@gmail.com>
2012-04-28 18:06:03 -0400

    When you put in a test, run it to make sure it doesn't fail.

--- predict.R
+++ predict.R
@@ -6,16 +6,18 @@ submit <- read.csv('data/SubmissionZerosExceptNAs.csv')
 
 library(plyr)
 
-medians <- colwise(function (x) median(x, na.rm=T), is.numeric)(train);
+rmedian <- function (x) median(x, na.rm=T);
+
+medians <- colwise(rmedian, is.numeric)(train);
 
 preds <- lapply(c('hour', 'chunkID', 'month_most_common'), function (var) {
-  medians.by.var <- ddply(train, var, colwise(function (x) median(x, na.rm=T), is.numeric));
+  medians.by.var <- ddply(train, var, colwise(rmedian, is.numeric));
   join(submit[,1:5], medians.by.var, by=var);
 });
 
 for (target in colnames(submit)[6:44]) {
   idxs <- which(submit[,target] > -999999);
-  submit[idxs, target] <- apply(sapply(preds, function (pred) pred[idxs,target]), 1, median);
+  submit[idxs, target] <- apply(sapply(preds, function (pred) pred[idxs,target]), 1, rmedian);
 }
 
 argv <- commandArgs(T);
