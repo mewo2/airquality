c0e3ed749d845f05e79a3bd502fe28a8f6ca226b
Martin O'Leary <m.e.w.oleary@gmail.com>
2012-04-29 03:44:21 -0400

    Some crap about weekdays.

--- predict.R
+++ predict.R
@@ -7,6 +7,9 @@ submit <- read.csv('data/SubmissionZerosExceptNAs.csv')
 library(plyr);
 library(laeken);
 
+lagged.df <- data.frame(weekday=train$weekday, chunkID=train$chunkID, position_within_chunk=train$position_within_chunk+168);
+submit$weekday <- join(submit, lagged.df, by=c('chunkID', 'position_within_chunk'))$weekday;
+
 rmedian <- function (x) median(x, na.rm=T);
 
 medians <- colwise(rmedian, is.numeric)(train);
@@ -17,17 +20,21 @@ varsets <- list(
     'hour',
     'chunkID',
     'month_most_common',
+    'weekday',
     c('hour', 'chunkID'),
-    c('hour', 'month_most_common')
+    c('hour', 'month_most_common'),
+    c('hour', 'weekday'),
+    c('chunkID', 'weekday'),
+    c('month_most_common', 'weekday')
   );
 preds <- lapply(varsets, function (var) {
   medians.by.var <- ddply(train, var, colwise(rmedian, is.numeric));
   list(
-    submit=join(submit[,1:5], medians.by.var, by=var),
+    submit=join(submit[,c(1:5, 45)], medians.by.var, by=var),
     train=join(train[,1:6], medians.by.var, by=var)
     );
 });
-
+submit$weekday <- NULL;
 
 for (target in colnames(submit)[6:44]) {
   idxs <- which(submit[,target] > -999999);
